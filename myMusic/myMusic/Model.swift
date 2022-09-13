99d1de77402d8e53406bc481f57aea93accd60ee
Serg Sutyrin <Drago060580@gmail.com>
2022-09-13 12:05:08 +0400

    Создали проект без изменений

---
+++ myMusic/myMusic/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  myMusic
+//
+//  Created by  Сергей on 02.09.2022.
+//
+
+import Foundation
+
+struct Song {
+    let name : String
+    let albumName : String
+    let artistName : String
+    let imageName : String
+    let trackName : String
+}
